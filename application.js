7db4b51528a4aeebd80e9309252ad626cacabdfe
John <johnmarksjr@gmail.com>
2014-02-17 09:35:30 -0700

    Tried to make header more responsive.
    Fixed bug where the “Experience” page was showing on the “Work” page.

--- application.js
+++ application.js
@@ -135,7 +135,7 @@ $(document).ready(function() {
 	$("section .navigator #workbtn").click(function() {
 		$("section#experience").css('display', 'none');
 		$("section#work").css('display', 'block');
-	});
+	}).trigger('click');
 	$("section .navigator #expbtn").click(function() {
 		$("section#work").css('display', 'none');
 		$("section#experience").css('display', 'block');
